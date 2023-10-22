cb230d1c34cb6e669cf4201008cfa37572ed2099
jahid <jahid1o@outlook.com>
2023-10-22 10:51:10 +0600

    Modify js/index.js  - added mouseovere evebtlistener  - added event listener for resize button    which gets user input and creates a new grid    with user input

--- js/index.js
+++ js/index.js
@@ -6,15 +6,31 @@ function createGrid(size) {
         for (let column = 0; column < size; column++) {
             let columnGrid = document.createElement("div")
             columnGrid.style.cssText = `width: ${gridSize}; height: ${gridSize};`
+
+            columnGrid.addEventListener("mouseover", () => {
+                columnGrid.style.cssText += "background-color: grey;"
+            })
             gridContainer.appendChild(columnGrid)
         }
     }
 }
 
 function clearGrid() {
-    document.querySelectorAll("#grid-container").forEach(element => {
+    document.querySelectorAll("#grid-container > div").forEach(element => {
         element.remove()
     });
 }
 
 createGrid(16)
+
+const resizeBtn = document.querySelector("#grid-resize-btn")
+
+resizeBtn.addEventListener("click", () => {
+    let userInput = Number(prompt("Please enter grid size (only numbers between 16 and 100)"))
+    
+    while (isNaN(userInput) || userInput < 16 || userInput > 100) {
+        userInput = Number(prompt("Please enter grid size (only numbers between 16 and 100)"))
+    }
+    clearGrid()
+    createGrid(userInput)
+})
